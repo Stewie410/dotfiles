2b2f4a6809f23da2a8333135c0508c03d6936016
Alex Paarfus <stewie410@gmail.com>
2025-10-19 11:01:31 -0400

    fix(bash): bad import paths

--- bash/config/import.sh
+++ bash/config/import.sh
@@ -11,18 +11,16 @@ if [[ "$(uname --kernel-release)" == *"WSL2"* ]]; then
     source "${BASH_CONFIG_HOME}/modules/wsl/vpn.sh"
 fi
 
-source "${BASH_CONFIG_HOME}/aliases/core.sh"
-source "${BASH_CONFIG_HOME}/aliases/tools.sh"
-source "${BASH_CONFIG_HOME}/aliases/dev.sh"
+source "${BASH_CONFIG_HOME}/alias/core.sh"
+source "${BASH_CONFIG_HOME}/alias/tools.sh"
+source "${BASH_CONFIG_HOME}/alias/dev.sh"
 
 source "${BASH_CONFIG_HOME}/functions/bm.sh"
 source "${BASH_CONFIG_HOME}/functions/cheat.sh"
-source "${BASH_CONFIG_HOME}/functions/d2.sh"
 source "${BASH_CONFIG_HOME}/functions/mkcd.sh"
-source "${BASH_CONFIG_HOME}/functions/python.sh"
 
 [[ -s "${HOME}/.cargo/env" ]] &&
     source "${HOME}/.cargo/env"
 
 [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] &&
-    source "${SDKMAN_DIR}/bin/sdkmin-init.sh"
+    source "${SDKMAN_DIR}/bin/sdkman-init.sh"
