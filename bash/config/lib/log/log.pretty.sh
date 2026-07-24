b2299effd6e4a9421482b91cba83b11d7e67349f
Alex Paarfus <stewie410@gmail.com>
2026-07-24 13:22:44 -0400

    feat: updated logger libs to support better UX, nested log() formatting, etc.

--- bash/config/lib/log/log.pretty.sh
+++ bash/config/lib/log/log.pretty.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2119,SC2120
 
 # Try to determine when to print color
 #
@@ -7,96 +8,91 @@
 #
 # @exit bool
 __logger.use_color() {
-    # https://no-color.org/
     [[ -n "${NO_COLOR}" ]] && return 1
+    [[ -t 1 ]] || return 1
 
     case "${TERM:-dumb}" in
-        "xterm"* | "rxvt"* | "screen"* | "tmux"*) return 0 ;;
-        "ansi" | "linux" | "vt100" | "vt220") return 0 ;;
-        "alacritty" | "kitty") return 0 ;;
+        "dumb" | "")
+            return 1
+            ;;
     esac
 
-    [[ -t 1 ]] || return 1
     if command -v 'tput' &> /dev/null; then
-        (($(tput colors) > 0)) && return 0
+        local colors
+        colors="$(tput colors 2> /dev/null)"
+        ((${colors:-0} <= 0)) && return 1
     fi
 
     return 1
 }
 
+# Close logger file descriptors
+#
+# @return true
+__logger.cleanup() {
+    exec 93>&-
+    exec 96>&-
+    if ((LOGGER_EXTENDED_FD == 1)); then
+        exec 90>&-
+        exec 91>&-
+        exec 92>&-
+        exec 94>&-
+        exec 95>&-
+        exec 97>&-
+    fi
+    wait
+    return 0
+}
+
 # Try to set up logging facilities
 #
-# @global NO_COLOR?     Force-disable coloring
-# @global TERM?         Terminal name to determine color support
-#
-# @param logger         Logger name: logger.pretty|logger.systemd
-# @param preference     Color support preference: always|yes|never|no|auto
-# @param logfile?       Logfile path (logger.pretty only)
-#
-# shellcheck disable=SC2154
-__logger.init() {
-    # logger.systemd: no colors, no bespoke logfile
-    if [[ "${1,,}" == "logger.systemd" ]]; then
-        unset log nocolor
-        return 0
-    fi
-
-    # logger.pretty: color support
-    case "${2,,}" in
-        "always" | "yes") nocolor="0" ;;
-        "never" | "no") nocolor="1" ;;
-        "auto")
-            __logger.use_color
-            nocolor="$?"
+# @global LOGGER_USE_COLOR?     Color support preference, default auto
+# @global LOGGER_LOGFILE?       Logfile path
+log.init() {
+    case "${LOGGER_USE_COLOR,,}" in
+        "always" | "yes")
+            LOGGER_USE_COLOR="always"
+            ;;
+        "never" | "no")
+            LOGGER_USE_COLOR="never"
+            ;;
+        *)
+            LOGGER_USE_COLOR="never"
+            __logger.use_color && LOGGER_USE_COLOR="always"
             ;;
     esac
 
-    # logger.pretty: logfile
-    if ((EUID == 0)); then
-        log="/var/log/${log}"
-    else
-        # handle non-root logfiles
-        log="${HOME}/.local/logs/${log}"
+    if [[ -z "${LOGGER_LOGFILE}" ]]; then
+        case "$(id --user)" in
+            0) LOGGER_LOGFILE="/var/log" ;;
+            *) LOGGER_LOGFILE="${HOME}/.local/logs" ;;
+        esac
+
+        local name
+        name="${BASH_SOURCE[0]##*/}"
+        LOGGER_LOGFILE+="/${name%.*}/${name%.*}.log"
     fi
-    mkdir -p "${log%/*}"
-    touch -a "${log}"
+
+    (
+        set -e
+        mkdir --parents "${LOGGER_LOGFILE%/*}"
+        touch -a "${LOGGER_LOGFILE}"
+    ) || return 1
 
     return 0
 }
 
-declare -A __logger_levels=(
-    ["emerg"]='EMERGENCY'
-    ["alert"]='ALERT'
-    ["crit"]='CRITICAL'
-    ["err"]='ERROR'
-    ["warn"]='WARNING'
-    ["notice"]='NOTICE'
-    ["info"]='INFO'
-    ["debug"]='DEBUG'
-)
-
-declare -A __logger_colors=(
-    ["EMERGENCY"]='\e[1;31m'
-    ["ALERT"]='\e[1;36m'
-    ["CRITICAL"]='\e[1;33m'
-    ["ERROR"]='\e[0;31m'
-    ["WARNING"]='\e[0;33m'
-    ["NOTICE"]='\e[0;32m'
-    ["INFO"]='\e[1;37m'
-    ["DEBUG"]='\e[1;35m'
-)
-
 # Write formatted messages to stdout & [$log].
 # WARN: Message(s) from args will IGNORE stdin!
 #
-# @global log?      Append plaintext to path, if set (default: /dev/null)
-# @global nocolor?  If true, drop colors from stdout
-# @global debug?    If true, skip debug lines
-# @global err[]?    If declared, append ERROR-like logs to array
+# @global LOGGER_USE_COLOR? If "never", drop colors from TTY
+# @global LOGGER_DEBUG?     If "0", skip debug lines
+# @global LOGGER_ERR[]?     If declared, append error-like lines to array
+# @global LOGGER_LOGFILE?   Append plaintext logs to path, if set (default: /dev/null)
 #
-# @stdin file       Content to write out
+# @stdin file       line(s) to write out
 #
-# @param level      Log "level" (see $__logger_levels)
+# @param level      Log "level" as integer, character, short or long name
 # @param line[]?    Line(s) to write out
 #
 # @stdout logs[]    Formatted log strings
@@ -107,65 +103,253 @@ log() {
         return 0
     fi
 
-    local lvl rgb rst
-    lvl="${__logger_levels["${1,,}"]}"
-    rgb="${__logger_colors["$lvl"]}"
-    rst='\e[0m'
+    local rgb lvl
+    case "${1,,}" in
+        "0" | "m" | "emerg" | "emergency")
+            rgb='\e[1;31m'
+            lvl="EMERGENCY"
+            ;;
+        "1" | "a" | "alert")
+            rgb='\e[1;36m'
+            lvl="ALERT"
+            ;;
+        "2" | "c" | "crit" | "critical")
+            rgb='\e[1;33m'
+            lvl="CRITICAL"
+            ;;
+        "3" | "e" | "err" | "error")
+            rgb='\e[0;31m'
+            lvl="ERROR"
+            ;;
+        "4" | "w" | "warn" | "warning")
+            rgb='\e[0;33m'
+            lvl="WARNING"
+            ;;
+        "5" | "n" | "notice")
+            rgb='\e[0;32m'
+            lvl="NOTICE"
+            ;;
+        "6" | "i" | "info")
+            rgb='\e[1;37m'
+            lvl="INFO"
+            ;;
+        "7" | "d" | "dbg" | "debug")
+            ((LOGGER_DEBUG == 1)) || return 0
+            rgb='\e[1;35m'
+            lvl="DEBUG"
+            ;;
+    esac
 
-    # shellcheck disable=SC2154
-    [[ "${lvl}" == "DEBUG" && "${debug}" -ne 1 ]] && return 0
-    [[ -n "${nocolor}" ]] && unset rgb rst
+    [[ "${LOGGER_USE_COLOR}" == "always" ]] || unset rgb
 
     local -a lines
     local line stamp
     while read -r line; do
         lines+=("${line}")
         printf -v stamp '%(%FT%T%z)T' -1
-        printf '%s|%b%-9s%b|%s\n' \
-            "${stamp}" "${rgb}" "${lvl}" "${rst}" "${line}"
-        printf '%s|%-9s|%s\n' \
-            "${stamp}" "${lvl}" "${line}" >> "${log:-/dev/null}"
+        printf '%s|%b%-9s\e[0m|%s\n' "${stamp}" "${rgb}" "${lvl}" "${line}"
+        printf '%s|%-9s|%s\n' "${stamp}" "${lvl}" "${line}" \
+            >> "${LOGGER_LOGFILE:-/dev/null}"
     done
 
-    # handle missing ${err[@]}
-    declare -p 'err' &> /dev/null \
-        || local -a err
+    declare -p 'LOGGER_ERR' &> /dev/null || return 0
 
     case "${lvl}" in
         "EMERGENCY" | "ALERT" | "CRITICAL" | "ERROR")
-            err+=("${lines[@]}")
+            LOGGER_ERR+=("${lines[@]}")
             ;;
     esac
 
     return 0
 }
 
-# Execute command & log cmd/args via log()
+# log() per-level helper functions
+# WARN: Message(s) from args will IGNORE stdin!
+#
+# @global LOGGER_USE_COLOR? If "never", drop colors from TTY
+# @global LOGGER_DEBUG?     If "0", skip debug lines
+# @global LOGGER_ERR[]?     If declared, append error-like lines to array
+# @global LOGGER_LOGFILE?   Append plaintext logs to path, if set (default: /dev/null)
+#
+# @stdin file       line(s) to write out
+#
+# @param line[]?    Line(s) to write out
+#
+# @stdout logs[]?   Formatted log strings
+# @exit true
+emergency() {
+    log "emerg" "${@}" >&2
+}
+
+alert() {
+    log "alert" "${@}" >&2
+}
+
+critical() {
+    log "crit" "${@}" >&2
+}
+
+error() {
+    log "err" "${@}" >&2
+}
+
+warning() {
+    log "warn" "${@}" >&2
+}
+
+notice() {
+    log "notice" "${@}"
+}
+
+info() {
+    log "info" "${@}"
+}
+
+debug() {
+    log "debug" "${@}"
+}
+
+# Early exit script (99), write cause as log.emergency()
+#
+# @param line[]?    Cause of early exit
+#
+# @stderr           Cause of early exit
+#
+# @exit 99 (false)
+die() {
+    set -- "${1:-Unknown Cause}" "${@:2}"
+    emergency "${1:-Unknown Cause}" "${@:2}"
+    exit 99
+}
+
+# Write to-be-executed command as quoted string
+#
+# @param command    command to be executed
+# @param args[]?    command arguments
+#
+# @return true
+__logger.exec_debug() {
+    local dbg
+    printf -v dbg ' %q' "${@}"
+    debug "EXEC: ${dbg:1}"
+    return 0
+}
+
+# Run a command, write stdout with info() (fd96), write stderr with error() (fd93)
 #
 # @stdin file?      Depends on command
 #
 # @param command    Command to execute
 # @param args[]?    Depends on command
 #
-# @stdout           Command's normal stdout
-# @stderr           Command's normal stderr
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
 # @exit int         Command's exit code
-log.exec() {
-    local dbg
-    printf -v dbg ' %q' "${@}"
-    log debug "EXEC: ${dbg:1}"
-
-    "${1}" "${@:2}"
+exec_log() {
+    __logger.exec_debug "${@}"
+    "${@}" 1>&96 2>&93
 }
 
-# Log message(s) as emergency with log(), then early-fail script
+# Run a command, write stdout with info() (fd96), leave stderr unchanged (fd2)
 #
-# @param line?      Line(s) to write as failure reason (default: Unknown Cause)
+# @stdin file?      Depends on command
 #
-# @stderr logs[]    Die reason(s)
-# @exit false
-die() {
-    set -- "${1:-Unknown Cause}" "${@:2}"
-    log emerg "${@}"
-    exit 1
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's unchanged stderr
+# @exit int         Command's exit code
+exec_stdout() {
+    __logger.exec_debug "${@}"
+    "${@}" 1>&96
 }
+
+# Run a command, leave stdout unchanged (fd1), write stderr with error() (fd93)
+#
+# @stdin file?      Depends on command
+#
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
+# @exit int         Command's exit code
+exec_stderr() {
+    __logger.exec_debug "${@}"
+    "${@}" 2>&93
+}
+
+# Run a nested script with own log() function, deduplicating formatting
+# Write all unformatted output with debug()
+#
+# @stdin file?      Depends on command
+#
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
+# @exit int         Command's exit code
+exec_nested() {
+    __logger.exec_debug "${@}"
+
+    local line
+    "${@}" 2>&1 | while read -r line; do
+        local lvl is_formatted
+        ((lvl = -1, is_formatted = 0))
+
+        case "${line}" in
+            *'|'*"|"*)
+                lvl="${line#*|}"
+                lvl="${lvl%%|*}"
+                lvl="${lvl%%[[:space:]]*}"
+
+                case "${lvl}" in
+                    "emergency" | \
+                        "alert" | \
+                        "critical" | \
+                        "error" | \
+                        "warning" | \
+                        "notice" | \
+                        "info" | \
+                        "debug")
+                        is_formatted="1"
+                        ;;
+                esac
+                ;;
+        esac
+
+        if ((is_formatted == 1)); then
+            local msg
+            msg="${line#*|}"
+            msg="${msg#*|}"
+
+            case "${lvl,,}" in
+                "info" | "notice" | "debug")
+                    log "${lvl}" "${1}: ${msg}"
+                    ;;
+                *)
+                    log "${lvl}" "${1}: ${msg}" >&2
+                    ;;
+            esac
+        else
+            debug "${1}: ${line}"
+        fi
+    done
+
+    return "${PIPESTATUS[0]}"
+}
+
+# Setup logger file descriptors, if enabled
+exec 93> >(error)
+exec 96> >(info)
+
+if ((LOGGER_EXTENDED_FD == 1)); then
+    exec 90> >(emergency)
+    exec 91> >(alert)
+    exec 92> >(critical)
+    exec 94> >(warning)
+    exec 95> >(notice)
+    exec 97> >(debug)
+fi
--- bash/config/lib/log/log.systemd.sh
+++ bash/config/lib/log/log.systemd.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2119,SC2120
 
-declare -A __logger_levels=(
-    ["emerg"]='0'
-    ["alert"]='1'
-    ["crit"]='2'
-    ["err"]='3'
-    ["warn"]='4'
-    ["notice"]='5'
-    ["info"]='6'
-    ["debug"]='7'
-)
+# Close logger file descriptors
+#
+# @return true
+__logger.cleanup() {
+    exec 93>&-
+    exec 96>&-
+    if ((LOGGER_EXTENDED_FD == 1)); then
+        exec 90>&-
+        exec 91>&-
+        exec 92>&-
+        exec 94>&-
+        exec 95>&-
+        exec 97>&-
+    fi
+    wait
+    return 0
+}
 
-# Write formatted messages to journald (/dev/fd/4)
+# Write formatted messages to stdout & [$log].
 # WARN: Message(s) from args will IGNORE stdin!
 #
-# @global debug?    If true, skip debug lines
-# @global err[]?    If declared, append ERROR-like logs to array
+# @global LOGGER_USE_COLOR? If "never", drop colors from TTY
+# @global LOGGER_DEBUG?     If "0", skip debug lines
+# @global LOGGER_ERR[]?     If declared, append error-like lines to array
+# @global LOGGER_LOGFILE?   Append plaintext logs to path, if set (default: /dev/null)
 #
-# @stdin file       Content to write out
+# @stdin file       line(s) to write out
 #
-# @param level      Log "level" (see $__logger_levels)
+# @param level      Log "level" as integer, character, short or long name
 # @param line[]?    Line(s) to write out
 #
+# @stdout logs[]    Formatted log strings
 # @exit true
 log() {
     if (($# > 1)); then
@@ -30,10 +41,33 @@ log() {
     fi
 
     local id
-    id="${__logger_levels["${1,,}"]}"
-
-    # shellcheck disable=SC2154
-    ((id == 7 && debug != 1)) && return 0
+    case "${1,,}" in
+        "0" | "m" | "emerg" | "emergency")
+            id="0"
+            ;;
+        "1" | "a" | "alert")
+            id="1"
+            ;;
+        "2" | "c" | "crit" | "critical")
+            id="2"
+            ;;
+        "3" | "e" | "err" | "error")
+            id="3"
+            ;;
+        "4" | "w" | "warn" | "warning")
+            id="4"
+            ;;
+        "5" | "n" | "notice")
+            id="5"
+            ;;
+        "6" | "i" | "info")
+            id="6"
+            ;;
+        "7" | "d" | "dbg" | "debug")
+            ((LOGGER_DEBUG == 1)) || return 0
+            id="7"
+            ;;
+    esac
 
     local -a lines
     local line
@@ -42,41 +76,201 @@ log() {
         printf '<%d>%s\n' "${id}" "${line}" >&4
     done
 
-    # handle missing ${err[@]}
     declare -p 'err' &> /dev/null \
-        || local -a err
+        || return 0
 
     ((id <= 3)) && err+=("${lines[@]}")
 
     return 0
 }
 
-# Execute command & log cmd/args via log()
+# log() per-level helper functions
+# WARN: Message(s) from args will IGNORE stdin!
+#
+# @global LOGGER_USE_COLOR? If "never", drop colors from TTY
+# @global LOGGER_DEBUG?     If "0", skip debug lines
+# @global LOGGER_ERR[]?     If declared, append error-like lines to array
+# @global LOGGER_LOGFILE?   Append plaintext logs to path, if set (default: /dev/null)
+#
+# @stdin file       line(s) to write out
+#
+# @param line[]?    Line(s) to write out
+#
+# @stdout logs[]?   Formatted log strings
+# @exit true
+emergency() {
+    log "emerg" "${@}"
+}
+
+alert() {
+    log "alert" "${@}"
+}
+
+critical() {
+    log "crit" "${@}"
+}
+
+error() {
+    log "err" "${@}"
+}
+
+warning() {
+    log "warn" "${@}"
+}
+
+notice() {
+    log "notice" "${@}"
+}
+
+info() {
+    log "info" "${@}"
+}
+
+debug() {
+    log "debug" "${@}"
+}
+
+# Early exit script (99), write cause as log.emergency()
+#
+# @param line[]?    Cause of early exit
+#
+# @stderr           Cause of early exit
+#
+# @exit 99 (false)
+die() {
+    set -- "${1:-Unknown Cause}" "${@:2}"
+    emergency "${1:-Unknown Cause}" "${@:2}"
+    exit 99
+}
+
+# Write to-be-executed command as quoted string
+#
+# @param command    command to be executed
+# @param args[]?    command arguments
+#
+# @return true
+__logger.exec_debug() {
+    local dbg
+    printf -v dbg ' %q' "${@}"
+    debug "EXEC: ${dbg:1}"
+    return 0
+}
+
+# Run a command, write stdout with info() (fd96), write stderr with error() (fd93)
 #
 # @stdin file?      Depends on command
 #
 # @param command    Command to execute
 # @param args[]?    Depends on command
 #
-# @stdout           Command's normal stdout
-# @stderr           Command's normal stderr
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
 # @exit int         Command's exit code
-log.debug() {
-    local dbg
-    printf -v dbg ' %q' "${@}"
-    log debug "EXEC: ${dbg:1}"
-
-    "${1}" "${@:2}"
+exec_log() {
+    __logger.exec_debug "${@}"
+    "${@}" 1>&96 2>&93
 }
 
-# Log message(s) as emergency with log(), then early-fail script
+# Run a command, write stdout with info() (fd96), leave stderr unchanged (fd2)
 #
-# @param line?      Line(s) to write as failure reason (default: Unknown Cause)
+# @stdin file?      Depends on command
 #
-# @stderr logs[]    Die reason(s)
-# @exit false
-die() {
-    set -- "${1:-Unknown Cause}" "${@:2}"
-    log debug "${@}"
-    exit 1
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's unchanged stderr
+# @exit int         Command's exit code
+exec_stdout() {
+    __logger.exec_debug "${@}"
+    "${@}" 1>&96
 }
+
+# Run a command, leave stdout unchanged (fd1), write stderr with error() (fd93)
+#
+# @stdin file?      Depends on command
+#
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
+# @exit int         Command's exit code
+exec_stderr() {
+    __logger.exec_debug "${@}"
+    "${@}" 2>&93
+}
+
+# Run a nested script with own log() function, deduplicating formatting
+# Write all unformatted output with debug()
+#
+# @stdin file?      Depends on command
+#
+# @param command    Command to execute
+# @param args[]?    Command arguments
+#
+# @stdout           Command's formatted stdout
+# @stderr           Command's formatted stderr
+# @exit int         Command's exit code
+exec_nested() {
+    __logger.exec_debug "${@}"
+
+    local line
+    "${@}" 2>&1 | while read -r line; do
+        local lvl is_formatted
+        ((lvl = -1, is_formatted = 0))
+
+        case "${line}" in
+            *'|'*"|"*)
+                lvl="${line#*|}"
+                lvl="${lvl%%|*}"
+                lvl="${lvl%%[[:space:]]*}"
+
+                case "${lvl}" in
+                    "emergency" | \
+                        "alert" | \
+                        "critical" | \
+                        "error" | \
+                        "warning" | \
+                        "notice" | \
+                        "info" | \
+                        "debug")
+                        is_formatted="1"
+                        ;;
+                esac
+                ;;
+        esac
+
+        if ((is_formatted == 1)); then
+            local msg
+            msg="${line#*|}"
+            msg="${msg#*|}"
+
+            case "${lvl,,}" in
+                "info" | "notice" | "debug")
+                    log "${lvl}" "${1}: ${msg}"
+                    ;;
+                *)
+                    log "${lvl}" "${1}: ${msg}" >&2
+                    ;;
+            esac
+        else
+            debug "${1}: ${line}"
+        fi
+    done
+
+    return "${PIPESTATUS[0]}"
+}
+
+# Setup logger file descriptors, if enabled
+exec 93> >(error)
+exec 96> >(info)
+
+if ((LOGGER_EXTENDED_FD == 1)); then
+    exec 90> >(emergency)
+    exec 91> >(alert)
+    exec 92> >(critical)
+    exec 94> >(warning)
+    exec 95> >(notice)
+    exec 97> >(debug)
+fi
